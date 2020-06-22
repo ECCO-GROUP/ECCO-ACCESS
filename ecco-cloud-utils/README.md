7be9035a879bc7589305a9948bfc825d0fe96834
ifenty <ifenty@gmail.com>
2020-06-22 09:20:48 -0700

    ecco_cloud_utils add

---
+++ ecco-cloud-utils/README.md
@@ -0,0 +1,19 @@
+## Synopsis
+
+ecco-cloud-utils is a Python package that includes tools for the ECCO-CLOUD system.
+
+## Installation
+
+git clone
+
+## Contributors
+
+If you would like to contribute, consider forking this repository and making pull requests via git.
+
+## Support 
+
+contact Ian.Fenty at jpl.nasa.gov
+
+## License
+
+MIT License
---
+++ ecco-cloud-utils/ecco_cloud_utils/__init__.py
@@ -0,0 +1,14 @@
+from .date_time import make_time_bounds_from_ds64
+from .date_time import extract_yyyy_mm_dd_hh_mm_ss_from_datetime64
+from .date_time import months2days
+
+from .llc_array_conversion  import llc_tiles_to_compact
+from .llc_array_conversion  import llc_tiles_to_faces
+from .llc_array_conversion  import llc_faces_to_compact
+
+from .records import make_empty_record
+from .records import save_to_disk
+
+from .mapping import find_mappings_from_source_to_target
+
+__all__ = ['date_time', 'llc_array_conversion', 'records', 'mapping']
