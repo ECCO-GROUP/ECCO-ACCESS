f007c0c6a00639fd68e488ba613f853fad2d48ff
kevinmarlis <kevinmarlis@gmail.com>
2021-08-30 11:49:54 -0700

    Fixed bug in extracting seconds from datetime object

--- ecco-cloud-utils/ecco_cloud_utils/date_time.py
+++ ecco-cloud-utils/ecco_cloud_utils/date_time.py
@@ -10,7 +10,8 @@ import dateutil as dateutil
 import numpy as np
 from datetime import datetime
 
-#%%
+# %%
+
 
 def make_time_bounds_from_ds64(rec_avg_end, output_freq_code):
     """
@@ -22,7 +23,7 @@ def make_time_bounds_from_ds64(rec_avg_end, output_freq_code):
     with two datetime64 variables, one for the averaging period start, one
     for the averaging period end.  Also find the middle time between the
     two..
-    
+
     Parameters
     ----------    
     rec_avg_end : numpy.datetime64 
@@ -37,57 +38,58 @@ def make_time_bounds_from_ds64(rec_avg_end, output_freq_code):
     -------
     time_bnds : numpy.array(dtype=numpy.datetime64)
         a datetime64 array with the start and end time of the averaging periods
-    
+
     center_times : numpy.datetime64
         the 'center' of the averaging period
-    
-    """ 
-    
-    if  output_freq_code in ('AVG_MON','AVG_DAY','AVG_WEEK','AVG_YEAR'):
+
+    """
+
+    if output_freq_code in ('AVG_MON', 'AVG_DAY', 'AVG_WEEK', 'AVG_YEAR'):
         rec_year, rec_mon, rec_day, \
-        rec_hour, rec_min, rec_sec = \
+            rec_hour, rec_min, rec_sec = \
             extract_yyyy_mm_dd_hh_mm_ss_from_datetime64(rec_avg_end)
-        
-        
-        rec_avg_end_as_dt = datetime(rec_year, rec_mon, 
+
+        rec_avg_end_as_dt = datetime(rec_year, rec_mon,
                                      rec_day, rec_hour,
                                      rec_min, rec_sec)
-        
-        if output_freq_code     == 'AVG_MON':
-            rec_avg_start =  rec_avg_end_as_dt - \
-                dateutil.relativedelta.relativedelta(months=1)    
-        elif output_freq_code   == 'AVG_DAY':
-            rec_avg_start =  rec_avg_end_as_dt - \
-                dateutil.relativedelta.relativedelta(days=1)  
-        elif output_freq_code   == 'AVG_WEEK':
-            rec_avg_start =  rec_avg_end_as_dt - \
-                dateutil.relativedelta.relativedelta(weeks=1)  
-        elif output_freq_code   == 'AVG_YEAR':
-            rec_avg_start =  rec_avg_end_as_dt - \
-                dateutil.relativedelta.relativedelta(years=1)    
 
-        rec_avg_start =  np.datetime64(rec_avg_start)
-        
+        if output_freq_code == 'AVG_MON':
+            rec_avg_start = rec_avg_end_as_dt - \
+                dateutil.relativedelta.relativedelta(months=1)
+        elif output_freq_code == 'AVG_DAY':
+            rec_avg_start = rec_avg_end_as_dt - \
+                dateutil.relativedelta.relativedelta(days=1)
+        elif output_freq_code == 'AVG_WEEK':
+            rec_avg_start = rec_avg_end_as_dt - \
+                dateutil.relativedelta.relativedelta(weeks=1)
+        elif output_freq_code == 'AVG_YEAR':
+            rec_avg_start = rec_avg_end_as_dt - \
+                dateutil.relativedelta.relativedelta(years=1)
+
+        rec_avg_start = np.datetime64(rec_avg_start)
+
         rec_avg_delta = rec_avg_end - rec_avg_start
         rec_avg_middle = rec_avg_start + rec_avg_delta/2
-        #print rec_avg_end, rec_avg_start, rec_avg_middle
-        
+        # print rec_avg_end, rec_avg_start, rec_avg_middle
+
         rec_time_bnds = np.array([rec_avg_start, rec_avg_end])
-        
+
         return rec_time_bnds, rec_avg_middle
-    
+
     else:
-        print ('output_freq_code must be: AVG_MON, AVG_DAY, AVG_WEEK, OR AVG_YEAR')
-        print ('you provided ' + str(output_freq_code))
-        return [],[]   
-    
-#%%
+        print('output_freq_code must be: AVG_MON, AVG_DAY, AVG_WEEK, OR AVG_YEAR')
+        print('you provided ' + str(output_freq_code))
+        return [], []
+
+# %%
+
+
 def extract_yyyy_mm_dd_hh_mm_ss_from_datetime64(dt64):
     """
 
     Extract separate fields for year, monday, day, hour, min, sec from
     a datetime64 object
-    
+
     Parameters
     ----------    
     dt64 : numpy.datetime64 
@@ -98,26 +100,25 @@ def extract_yyyy_mm_dd_hh_mm_ss_from_datetime64(dt64):
     year, mon, day, hh, mm, ss : int
 
     """
-    
+
     s = str(dt64)
     year = int(s[0:4])
     mon = int(s[5:7])
     day = int(s[8:10])
     hh = int(s[11:13])
     mm = int(s[14:16])
-    ss = int(s[17:18])
-    
-    #print year, mon, day, hh, mm, ss
-    return year,mon,day,hh,mm,ss 
+    ss = int(s[17:19])
+
+    # print year, mon, day, hh, mm, ss
+    return year, mon, day, hh, mm, ss
 
 
-
-#%%
+# %%
 def months2days(nmon=288, baseyear=1992, basemon=1):
     """ 
 
     This routine converts the mid-month time to days from January 1st of a particular year.
-    
+
     Parameters
     ----------
     nmon : dtype=integer
@@ -125,15 +126,15 @@ def months2days(nmon=288, baseyear=1992, basemon=1):
 
     baseyear : dtype=integer
         year of time of origin
-        
+
     basemon : dtype=integer
         month of time of origin 
-    
+
     Returns
     -------
         time_days : ndarray
             the middle time of each month in days from Jan 1 baseyear (numpy array [nmon], dtype=double)
-        
+
         time_days_bnds : ndarray
             time bounds (numpy array [nmon, 2], dtype=double)
 
@@ -141,28 +142,30 @@ def months2days(nmon=288, baseyear=1992, basemon=1):
             array of ANSI date strings
 
     """
-    
-    time_days_bnds = np.zeros([nmon,2])
+
+    time_days_bnds = np.zeros([nmon, 2])
     time_1stdayofmon = np.zeros([nmon+1])
-    
+
     basetime = datetime.datetime(baseyear, basemon, 1, 0, 0, 0)
 
     for mon in range(nmon+1):
-        #monfrombasemon is how many months fron basemon
-        monfrombasemon=basemon+mon-1
+        # monfrombasemon is how many months fron basemon
+        monfrombasemon = basemon+mon-1
         yrtmp = monfrombasemon//12+baseyear
-        montmp = monfrombasemon%12+1
-        tmpdate = datetime.datetime(yrtmp,montmp,1,0,0,0)-basetime
+        montmp = monfrombasemon % 12+1
+        tmpdate = datetime.datetime(yrtmp, montmp, 1, 0, 0, 0)-basetime
         time_1stdayofmon[mon] = tmpdate.days
-    #time bounds are the 1st day of each month.
-    time_days_bnds[:,0]= time_1stdayofmon[0:nmon]
-    time_days_bnds[:,1]= time_1stdayofmon[1:nmon+1]
-    #center time of each month is the mean of the time bounds.    
-    time_days = np.mean(time_days_bnds,axis=1)
+    # time bounds are the 1st day of each month.
+    time_days_bnds[:, 0] = time_1stdayofmon[0:nmon]
+    time_days_bnds[:, 1] = time_1stdayofmon[1:nmon+1]
+    # center time of each month is the mean of the time bounds.
+    time_days = np.mean(time_days_bnds, axis=1)
 
-    ansi_datetmp = np.array([basetime + datetime.timedelta(days=time_days[i]) for i in xrange(nmon)])
-    ansi_date = [str.replace(ansi_datetmp[i].isoformat(),'T',' ') for i in range(nmon)]
+    ansi_datetmp = np.array(
+        [basetime + datetime.timedelta(days=time_days[i]) for i in xrange(nmon)])
+    ansi_date = [str.replace(ansi_datetmp[i].isoformat(), 'T', ' ')
+                 for i in range(nmon)]
 
     return time_days, time_days_bnds, ansi_date
 
-#%%
+# %%
