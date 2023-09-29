e4912ad0db3199fa5d65b65e1b3259d374585783
Greg Moore <greg.moore@jpl.nasa.gov>
2023-09-28 20:45:49 -0700

    Change deprecated hyphens ("-") to underscores ("_") in setup, added installation package requirements

--- ecco-cloud-utils/setup.py
+++ ecco-cloud-utils/setup.py
@@ -3,17 +3,24 @@ import setuptools
 from distutils.core import setup
 
 setup(
-  name = 'ecco-cloud-utils',
-  packages = ['ecco-cloud-utils'], # this must be the same as the name above
+  name = 'ecco_cloud_utils',
+  packages = ['ecco_cloud_utils'], # this must be the same as the name above
   version = '0.0.2',
   description = 'Estimating the Circulation and Climate of the Ocean (ECCO) - Cloud Package',
   author = 'Ian Fenty',
   author_email = 'ian.fenty@jpl.nasa.gov',
-  url = 'https://github.com/ECCO-GROUP/ECCO-CLOUD/ecco-cloud-utils',
+  url = 'https://github.com/ECCO-GROUP/ECCO-ACCESS/tree/master/ecco-cloud-utils',
   keywords = ['ecco','mitgcm','estimate','circulation','climate'],
   include_package_data=True,
  # data_files=[('binary_data',['binary_data/basins.data', 'binary_data/basins.meta'])],
-  install_requires=['datetime', 'python-dateutil', 'numpy'],
+  install_requires=[
+    'datetime',
+    'pyresample',
+    'python-dateutil',
+    'netCDF4',
+    'numpy',
+    'xarray'
+  ],
   classifiers=[
       'Development Status :: 5 - Production/Stable',
       'Intended Audience :: Science/Research', 
