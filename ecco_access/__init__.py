d4a2a75c35d168c5b0d3667ee88b99c41ca41002
Andrew Delman <andrew.s.delman@gmail.com>
2025-10-08 19:29:11 +0000

    modify __init__.py for ecco_access package

--- ecco_access/__init__.py
+++ ecco_access/__init__.py
@@ -1,97 +1,12 @@
-from .calc_meridional_trsp import calc_meridional_vol_trsp, \
-        calc_meridional_heat_trsp, calc_meridional_salt_trsp, \
-        meridional_trsp_at_depth
-
-from .calc_section_trsp import calc_section_vol_trsp, \
-        calc_section_heat_trsp, calc_section_salt_trsp, \
-        section_trsp_at_depth
-
-from .calc_stf import calc_meridional_stf, calc_section_stf
-
-from .ecco_utils import make_time_bounds_and_center_times_from_ecco_dataset
-from .ecco_utils import make_time_bounds_from_ds64
-from .ecco_utils import extract_yyyy_mm_dd_hh_mm_ss_from_datetime64
-
-from .ecco_utils import minimal_metadata
-from .ecco_utils import months2days
-from .ecco_utils import get_llc_grid
-from .ecco_utils import find_metadata_in_json_dictionary
-from .ecco_utils import add_global_metadata
-from .ecco_utils import add_coordinate_metadata
-from .ecco_utils import add_variable_metadata
-from .ecco_utils import sort_attrs
-from .ecco_utils import sort_all_attrs
-
-from .get_basin import get_basin_mask, get_available_basin_names
-
-from .get_section_masks import get_section_endpoints, \
-        get_available_sections, get_section_line_masks
-
-#from extract_grid_fields_from_mitgrid_files import extract_U_point_grid_fields_from_mitgrid_as_tiles
-#from extract_grid_fields_from_mitgrid_files import extract_G_point_grid_fields_from_mitgrid_as_tiles
-
-from .llc_array_conversion  import llc_compact_to_tiles
-from .llc_array_conversion  import llc_tiles_to_compact
-from .llc_array_conversion  import llc_compact_to_faces
-from .llc_array_conversion  import llc_faces_to_tiles
-from .llc_array_conversion  import llc_ig_jg_faces_to_tiles
-from .llc_array_conversion  import llc_tiles_to_faces
-from .llc_array_conversion  import llc_faces_to_compact
-from .llc_array_conversion  import llc_tiles_to_xda
-
-#from .netcdf_product_generation import create_nc_grid_files_on_native_grid_from_mds
-from .netcdf_product_generation import get_time_steps_from_mds_files
-#from .netcdf_product_generation import create_nc_variable_files_on_native_grid_from_mds
-from .netcdf_product_generation import create_nc_variable_files_on_regular_grid_from_mds
-#from .netcdf_product_generation import update_ecco_dataset_geospatial_metadata
-#from .netcdf_product_generation import update_ecco_dataset_temporal_coverage_metadata
-from .netcdf_product_generation import save_ecco_dataset_to_netcdf
-from .netcdf_product_generation import make_date_str_from_dt64
-
-from .read_bin_llc import read_llc_to_tiles
-from .read_bin_llc import read_llc_to_compact
-from .read_bin_llc import read_llc_to_faces
-from .read_bin_llc import load_ecco_vars_from_mds
-from .read_bin_gen import load_binary_array
-
-from .resample_to_latlon import resample_to_latlon
-
-from ecco_v4_py import  scalar_calc
-
-from .tile_io import load_ecco_grid_nc
-from .tile_io import load_ecco_var_from_years_nc
-from .tile_io import recursive_load_ecco_var_from_years_nc
-
-from .tile_plot import plot_tile
-from .tile_plot import plot_tiles
-from .tile_plot import unique_color
-
-from .tile_plot_proj import plot_proj_to_latlon_grid
-from .tile_plot_proj import plot_pstereo
-from .tile_plot_proj import plot_global
-
-from ecco_v4_py import vector_calc
-
 from .ecco_access import ecco_podaac_access
 from .ecco_access import ecco_podaac_to_xrdataset
 
-__all__ = ['calc_meridional_trsp',
-           'calc_section_trsp',
-           'calc_stf',
-           'ecco_acc_dates',
+from .ecco_download import setup_earthdata_login_auth
+from .ecco_s3_retrieve import init_S3FileSystem
+
+
+__all__ = ['ecco_acc_dates',
            'ecco_access',
            'ecco_download',
            'ecco_s3_retrieve',
-           'ecco_utils',
-           'ecco_varlist',
-           'get_section_masks',
-           'llc_array_conversion',
-           'netcdf_product_generation',
-           'read_bin_llc',
-           'read_bin_gen',
-           'resample_to_latlon',
-           'scalar_calc',
-           'tile_io',
-           'tile_plot',
-           'tile_plot_proj',
-           'vector_calc']
+           'ecco_varlist']
