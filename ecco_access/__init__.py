6a2fb532546147bc79fe56a0d993b483ae46013e
Andrew Delman <andrew.s.delman@gmail.com>
2025-10-14 23:30:06 +0000

    added download_subset function to __init__.py

--- ecco_access/__init__.py
+++ ecco_access/__init__.py
@@ -4,6 +4,8 @@ from .ecco_access import ecco_podaac_to_xrdataset
 from .ecco_download import setup_earthdata_login_auth
 from .ecco_s3_retrieve import init_S3FileSystem
 
+from .ecco_download import ecco_podaac_download_subset
+
 
 __all__ = ['ecco_acc_dates',
            'ecco_access',
